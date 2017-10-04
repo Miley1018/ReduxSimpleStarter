c62162a9ee893d153dcb705243f79fb3d4ac6506
Miley Gao <mileygao@Mileys-MacBook-Pro.local>
2017-10-04 12:11:11 -0700

    videoList

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY='AIzaSyAuWE485k51YR5F1hMLWw2aM7lg9zRAVJs';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {videos:[]};
+        YTSearch({key:API_KEY,term:'we'},(videos)=>{
+            this.setState({videos})
+        });
+    }
+    render(){
+        return ( <div>
+            <SearchBar/>
+            <VideoList videos={this.state.videos}/>
+        </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
